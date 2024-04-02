64210653ffe306c8872c87087b10cf505aa8934a
carsonkimck <carsonkim029@gmail.com>
2024-04-01 18:04:02 -0700

    added copy email button, contact section, fixed header

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,25 @@ document.getElementById("ProjectsButton").onclick = function() {
     });
 }
 
+
+document.getElementById("ContactButton").onclick = function() {
+  document.getElementById("contact").scrollIntoView({
+      behavior: "smooth"
+  });
+}
+document.getElementById('copyButton').addEventListener('click', function() {
+  // Copying logic here (you can replace this with your own logic)
+  const textToCopy = "ckbit@proton.me";
+  navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+          // Show success message
+          document.getElementById('successMessage').style.display = 'block';
+          // Hide success message after 2 seconds
+          setTimeout(function() {
+              document.getElementById('successMessage').style.display = 'none';
+          }, 2000);
+      })
+      .catch(err => {
+          console.error('Failed to copy: ', err);
+      });
+});
